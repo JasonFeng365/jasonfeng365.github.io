bada11783cb4f95a8d866285fa20e3aa52b25e8d
Jason Feng <34261800+JasonFeng365@users.noreply.github.com>
2023-01-08 15:33:42 -0800

    Projects update

--- js/projects.js
+++ js/projects.js
@@ -1,5 +1,5 @@
-function beginGroup(href, name){
-    return "<div class=\"card\">\n<div class=\"card-header\" data-bs-toggle=\"collapse\" href=\"#"+href+"\">\n<h4>"+name+"</h4>\n</div>\n<div id=\""+href+"\" class=\"collapse\">\n<div class=\"card-body\">\n"
+function beginGroup(href, name, desc){
+    return "<div class=\"card\">\n<div class=\"card-header\" data-bs-toggle=\"collapse\" href=\"#"+href+"\">\n<h4>"+name+"</h4>\n</div>\n<div id=\""+href+"\" class=\"collapse\">\n<div class=\"card-body\">\n<h5>"+desc+"</h5>\n"
 }
 
 function addCard(href, name, text){
@@ -10,31 +10,68 @@ function closeCard(){
     return "</div>\n</div>\n</div>\n"
 }
 
-function section(href, name, subHref, subNames, subText){
-    var out = beginGroup(href, name)
+function section(href, name, desc, subHref, subNames, subText){
+    var out = beginGroup(href, name, desc)
     for (var i = 0; i < subHref.length; i++)
         out+=addCard(subHref[i], subNames[i], subText[i])
     out+=closeCard()
     return out
 }
 
+function getGitPage(url){
+    return "<a href=\""+url+"\" target=\"_blank\">GitHub Page</a>"
+}
+
+function games(){
+    const href = "games"
+    const name = "Games"
+    const desc = "Computer games that I created over the years."
+    const subHref = ["cam3", "cam4", "pyb", "destinycraft", "cam2"]
+    const subNames = ["Cat and Mouse 3", "Cat and Mouse 4", "Pushing your Buttons", "DestinyCraft", "Cat and Mouse 2"]
+    const subText = ["Two-dimensional platformering fighter heavily inspired by Adobe Flash games of the past. Written in pure Java, using the Swing toolkit.",
+    "Work-in-progress sequel/remake of Cat and Mouse 3, written in Unity. Features will include an overhauled equipment system, new story, quality-of-life improvements, and restyled UI.",
+    "Simple platformer using Java Swing, with a twist: terrain can be modified. Written in pure Java, it is my first game outside of Scratch.",
+    "Work-in-progress Minecraft mod that adds items from Destiny 2, along with their special effects.",
+    "The original Cat and Mouse computer game, created in Scratch, and based off of a real-life game by friends and I played from a young age."]
+
+    return section(href, name, desc, subHref, subNames, subText)
+}
+
+
+
 function bots(){
     const href = "bots"
     const name = "Bots"
-    const subHref = ["kirbylorebot"]
-    const subNames = ["Kirby Lore Bot"]
-    const subText = ["Discord bot"]
+    const desc = "Various Discord bots I've coded with the <a href=\"https://github.com/DV8FromTheWorld/JDA\" target=\"_blank\">Java Discord API 4.4.0</a>."
+    const subHref = ["kirbylorebot", "sierracsbot", "tutorcenterbot"]
+    const subNames = ["Kirby Lore Bot", "Sierra CS Bot", "Tutor Center Bot"]
+    const subText = ["Multi-purpose Discord bot used by over 200 people. Features include image manipulation, an automatic information-sending system, reminders and scheduled message sending, weather information, and of course, 6,195 lines of Kirby lore.",
+    "Simple Discord bot to automate the Sierra College Computer Science Discord server, with role management for placing students in correct channel groups.",
+    "Work-in-progress bot that will display schedules of Sierra College tutors on a daily basis."]
 
-    return section(href, name, subHref, subNames, subText)
+    return section(href, name, desc, subHref, subNames, subText)
 }
 function apis(){
     const href = "apis"
     const name = "APIs"
+    const desc = "REST APIs I created to simplify complex data."
     const subHref = ["classsearchapi"]
     const subNames = ["Sierra College Class Search API"]
-    const subText = ["Rest API"]
+    const subText = ["Node.js REST API which returns the result of Sierra College's class search interface, as in JSON format. Although it is currently not hosted anywhere, I aim to find a host for it soon.<br>"+getGitPage("https://github.com/JasonFeng365/class-search-api")]
 
-    return section(href, name, subHref, subNames, subText)
+    return section(href, name, desc, subHref, subNames, subText)
+}
+
+function assistants(){
+    const href = "assistants"
+    const name = "Assistants"
+    const desc = "Programs to automate simple tasks or add ease to tasks unable to be automated."
+    const subHref = ["propagator", "taskmanager"]
+    const subNames = ["Error Propagator", "Task Manager"]
+    const subText = ["Work-in-progress macro that propagates the error of variables, given an equation. It generates the equations, and automatically types them with Microsoft Word formatting.<br>"+getGitPage("https://github.com/JasonFeng365/PropagationOfError"),
+    "Macro that completes Among Us crewmate tasks automatically, reducing the need for user input. Remember: if you can code the cheat, you deserve to use it. (This is a joke.)"]
+
+    return section(href, name, desc, subHref, subNames, subText)
 }
 
 
@@ -44,6 +81,6 @@ const projects = new Vue({
         allProjects:null
     },
     mounted: function() {
-        this.allProjects = bots()+apis()
+        this.allProjects = games()+bots()+apis()+assistants()
     }
 });
