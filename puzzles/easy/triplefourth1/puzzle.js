37c8321239b80d88d6af124c9b8f604653564700
Jason Feng <34261800+JasonFeng365@users.noreply.github.com>
2023-04-23 12:05:50 -0700

    Removal of unnecessary input files

--- puzzles/easy/triplefourth1/puzzle.js
+++ puzzles/easy/triplefourth1/puzzle.js
@@ -4,7 +4,7 @@ function generateTable(){
     let mag = [5, 4, 3, 3, 4, 3, 3, 2, 3, 3, 2, 1, 3, 2, 1, 1, 2, 1, 1, 0]
     let perks = [0, 0, 1, 2, 0, 1, 0, 2, 1, 0, 0, 3, 0, 0, 1, 2, 0, 1, 0]
     let perkNames = ["None", "<b>Triple</b>", "<b>Fourth</b>", "<b><i>Both</i></b>"]
-    let out = "<table class=\"table-responsive\"><table class=\"table\"><tbody><tr>"
+    let out = "<div class=\"table-responsive\"><table class=\"table\"><tbody><tr>"
 
     out += "<th>Shot</th>"
     for (let i = 1; i <= 20; i++) out += "<td>"+i+"</td>"
