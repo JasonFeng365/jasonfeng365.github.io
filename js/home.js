e89ba64f0971b79b3f2a05aea1f1f9fdbd5b4bba
JasonFeng365 <34261800+JasonFeng365@users.noreply.github.com>
2024-03-05 00:42:34 -0800

    Website revamp, March 2024

--- js/home.js
+++ js/home.js
@@ -1,21 +1,21 @@
-const puzzleTab = document.querySelector("#puzzleTab")
+const challengeTab = document.querySelector("#challengeTab")
 
 const home = new Vue({
-    el: '#home',
-    data: {
-        timeGreeting:"Good ",
-    },
-    methods:{
-        puzzleLink(){
-            puzzleTab.click();
-        }
-    },
-    mounted: function() {
-        try{
-        let hours =new Date().getHours();
-        if (hours < 12) this.timeGreeting+="morning!"
-        else if (hours < 18) this.timeGreeting+="afternoon!"
-        else this.timeGreeting+="evening!"
-        } catch (e) {this.timeGreeting = "Hello there!"}
-    }
+	el: '#home',
+	data: {
+		timeGreeting:"Good ",
+	},
+	methods:{
+		challenges(){
+			challengeTab.click();
+		}
+	},
+	mounted: function() {
+		try{
+		let hours =new Date().getHours();
+		if (hours < 12) this.timeGreeting+="morning!"
+		else if (hours < 18) this.timeGreeting+="afternoon!"
+		else this.timeGreeting+="evening!"
+		} catch (e) {this.timeGreeting = "Hello there!"}
+	}
 });
